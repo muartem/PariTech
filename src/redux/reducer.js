250bffd2cf327a1a358d523874bc721b09d69c1e
Artem Martyniuk <muartem99@gmail.com>
2021-03-12 06:40:59 +0200

    Delete src directory

--- src/redux/reducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-import {apiReducer} from "./apiReducer";
-import {loaderReducer} from "./loaderReducer";
-
-export const reducer = combineReducers({
-    api: apiReducer,
-    loader: loaderReducer
-})
--- src/redux/types.js
+++
@@ -1,7 +0,0 @@
-export const GET_PHOTOS  = 'API/GET_PHOTOS'
-export const GET_PHOTO  = 'API/GET_PHOTO'
-export const GET_USER  = 'API/GET_USER'
-export const RESET  = 'API/RESET'
-
-export const SHOW_LOADER = 'LOADER/SHOW_LOADER'
-export const HIDE_LOADER = 'LOADER/HIDE_LOADER'
